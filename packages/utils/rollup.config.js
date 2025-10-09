dc88511b940146f18c8d23a5ac13dee16ddd374a
kimdw524 <kimdw524@naver.com>
2025-10-09 21:44:53 +0900

    test

---
+++ packages/utils/rollup.config.js
@@ -0,0 +1,23 @@
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    dir: 'dist',
+    format: 'esm',
+    preserveModules: true,
+    entryFileNames: '[name].js',
+  },
+  plugins: [
+    resolve({ extensions: ['.js', '.ts', '.tsx'] }),
+
+    commonjs({
+      include: /node_modules/,
+      defaultIsModuleExports: true,
+    }),
+
+    typescript({ tsconfig: './tsconfig.json' }),
+  ],
+};
--- packages/utils/src/_internal/utils/isForwardRefComponent/index.ts
+++
@@ -1,7 +0,0 @@
-export const isForwardRefComponent = <P>(
-  component: (React.ComponentType<P> | React.ForwardRefExoticComponent<P>) & {
-    $$typeof?: symbol;
-  },
-): component is React.ForwardRefExoticComponent<P> => {
-  return component.$$typeof === Symbol.for('react.forward_ref');
-};
