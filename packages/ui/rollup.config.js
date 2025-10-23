d80b7159ff2873e5c2b4af7f46d50d6c61bd2756
kimdw524 <kimdw524@naver.com>
2025-10-24 05:44:15 +0900

    test

---
+++ packages/ui/rollup.config.js
@@ -0,0 +1,35 @@
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+import fs from 'fs';
+
+const pkg = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
+
+const peerDeps = pkg.peerDependencies ? Object.keys(pkg.peerDependencies) : [];
+export default {
+  input: 'src/index.ts',
+  output: {
+    dir: 'dist',
+    format: 'esm',
+    preserveModules: true,
+    preserveModulesRoot: './src',
+    entryFileNames: '[name].js',
+  },
+  treeshake: {
+    moduleSideEffects: false,
+    propertyReadSideEffects: false,
+    tryCatchDeoptimization: false,
+    unknownGlobalSideEffects: false,
+  },
+  external: peerDeps,
+  plugins: [
+    resolve({ extensions: ['.js', '.ts', '.tsx'] }),
+
+    commonjs({
+      include: /node_modules/,
+      defaultIsModuleExports: true,
+    }),
+
+    typescript({ tsconfig: './tsconfig.json' }),
+  ],
+};
