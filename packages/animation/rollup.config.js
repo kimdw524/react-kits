c6dffa89d6643ab7e49351fb6a9e9cca9838509a
kimdw524 <kimdw524@naver.com>
2025-12-10 03:56:53 +0900

    feat(animation): rollup preserve directives

--- packages/animation/rollup.config.js
+++ packages/animation/rollup.config.js
@@ -2,6 +2,7 @@ import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import fs from 'fs';
+import preserveDirectives from 'rollup-plugin-preserve-directives';
 
 const pkg = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
 
@@ -18,12 +19,11 @@ export default {
   external: [...peerDeps, 'react/jsx-runtime'],
   plugins: [
     resolve({ extensions: ['.js', '.ts', '.tsx'] }),
-
     commonjs({
       include: /node_modules/,
       defaultIsModuleExports: true,
     }),
-
+    preserveDirectives(),
     typescript({ tsconfig: './tsconfig.json' }),
   ],
 };
