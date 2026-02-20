96a0bb445d4de8963bd4b90571577cf6b916e46e
kimdw524 <kimdw524@naver.com>
2026-02-21 06:37:43 +0900

    chore(ui): SelectOptionList 최대 크기 제한

--- packages/ui/src/components/Select/SelectOption.css.ts
+++ packages/ui/src/components/Select/SelectOption.css.ts
@@ -2,7 +2,12 @@ import { styleWithLayer } from '#styleUtils';
 import { theme } from '#themes';
 
 export const selectOption = styleWithLayer({
+  overflowX: 'hidden',
+
   padding: '0.75em 0.5em',
+
+  textOverflow: 'ellipsis',
+  whiteSpace: 'nowrap',
 
   transition: 'background-color 0.2s ease',
 
